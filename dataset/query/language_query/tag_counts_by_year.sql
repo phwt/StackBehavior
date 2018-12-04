9626827ca2ea3c9e63a76b8e2c6dd501c2c0f8f7
phwt.smwt <phwt.smwt@gmail.com>
2018-12-04 21:02:58 +0700

    Change case to alphabetical order

--- dataset/query/language_query/tag_counts_by_year.sql
+++ dataset/query/language_query/tag_counts_by_year.sql
@@ -4,23 +4,23 @@ FROM (
     SELECT
         tags, creation_date,
     CASE
-        WHEN LOWER(tags) LIKE "%python%" THEN "python"
-        WHEN LOWER(tags) LIKE "%javascript%" THEN "javascript"
-        WHEN LOWER(tags) LIKE "%java%" THEN "java"
-        WHEN LOWER(tags) LIKE "%c#%" THEN "c#"
-        WHEN LOWER(tags) LIKE "%php%" THEN "php"
-        WHEN LOWER(tags) LIKE "%html%" THEN "html"
-        WHEN LOWER(tags) LIKE "%c++%" THEN "c++"
+        WHEN LOWER(tags) LIKE "%android%" THEN "android"
         WHEN LOWER(tags) LIKE "%angular%" THEN "angular"
+        WHEN LOWER(tags) LIKE "%c#%" THEN "c#"
+        WHEN LOWER(tags) LIKE "%c++%" THEN "c++"
+        WHEN LOWER(tags) LIKE "%html%" THEN "html"
+        WHEN LOWER(tags) LIKE "%ios%" THEN "ios"
+        WHEN LOWER(tags) LIKE "%iphone%" THEN "iphone"
+        WHEN LOWER(tags) LIKE "%java%" THEN "java"
+        WHEN LOWER(tags) LIKE "%javascript%" THEN "javascript"
         WHEN LOWER(tags) LIKE "%jquery%" THEN "jquery"
+        WHEN LOWER(tags) LIKE "%php%" THEN "php"
+        WHEN LOWER(tags) LIKE "%python%" THEN "python"
         WHEN LOWER(tags) LIKE "%sql%" THEN "sql"
         WHEN LOWER(tags) LIKE "%swift%" THEN "swift"
-        WHEN LOWER(tags) LIKE "%xcode%" THEN "xcode"
-        WHEN LOWER(tags) LIKE "%android%" THEN "android"
-        WHEN LOWER(tags) LIKE "%iphone%" THEN "iphone"
-        WHEN LOWER(tags) LIKE "%ios%" THEN "ios"
-        WHEN LOWER(tags) LIKE "%wordpress%" THEN "wordpress"
         WHEN LOWER(tags) LIKE "%windows%" THEN "windows"
+        WHEN LOWER(tags) LIKE "%wordpress%" THEN "wordpress"
+        WHEN LOWER(tags) LIKE "%xcode%" THEN "xcode"
         ELSE tags
     END
     FROM
@@ -30,5 +30,5 @@ FROM (
         EXTRACT(QUARTER FROM creation_date) = 1
 )
 WHERE
-  tags IN ('python', 'javascript', 'java', 'c#', 'php', 'html', 'c++', 'angular', 'jquery', 'sql', 'swift', 'xcode', 'android', 'iphone', 'ios', 'wordpress', 'windows')
+  tags IN ('android', 'angular', 'c#', 'c++', 'html', 'ios', 'iphone', 'java', 'javascript', 'jquery', 'php', 'python', 'sql', 'swift', 'windows', 'wordpress', 'xcode')
 GROUP BY tags
