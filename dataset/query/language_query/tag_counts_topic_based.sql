4bde409a429803557279899abf8455266b7e4b02
phwt.smwt <phwt.smwt@gmail.com>
2018-12-05 12:13:50 +0700

    Add variable for more flexibility

--- dataset/query/language_query/tag_counts_topic_based.sql
+++ dataset/query/language_query/tag_counts_topic_based.sql
@@ -1,14 +1,15 @@
 SELECT
     year, quarter, COUNT(*) AS total
 FROM(
+    WITH vars AS (SELECT 'python' AS topic)
     SELECT
         EXTRACT(YEAR FROM creation_date) AS year,
         EXTRACT(QUARTER FROM creation_date) AS quarter,
-        CASE WHEN LOWER(tags) LIKE "%python%" THEN "python" ELSE "" END
+        CASE WHEN LOWER(tags) LIKE CONCAT("%", (SELECT topic FROM vars), "%") THEN (SELECT topic FROM vars) ELSE "" END
     FROM
         `bigquery-public-data.stackoverflow.posts_questions`
     WHERE
-         tags LIKE "%python%"
+         tags LIKE CONCAT("%", (SELECT topic FROM vars), "%")
 )
 GROUP BY year, quarter
 ORDER BY year, quarter
