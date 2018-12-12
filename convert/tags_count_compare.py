ccd678f8fa24d2eecb4e0c45cc95e2ad0527e8f9
phwt.smwt <phwt.smwt@gmail.com>
2018-12-13 00:48:44 +0700

    Remove quarter supporting for files
    Co-Authored-By: ณภัทร  พรบุญเรือง <61070044@users.noreply.github.com>

--- convert/tags_count_compare.py
+++ convert/tags_count_compare.py
@@ -8,7 +8,8 @@ question_count = list()
 def readFile():
     """ Read the given csv file """
     getCount()
-    path = './dataset/data/tag_counts/tags_count_topic_based_merged.csv'
+    # path = './dataset/data/tag_counts/tags_count_topic_based_merged.csv'
+    path = './dataset/data/tag_counts/tags_count_topic_based_merged_year.csv'
     with open(path, encoding="utf8") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         # next(readCSV, None) #Skip header
@@ -28,21 +29,21 @@ def getCount():
 
 def compare(topics):
     """ Compare topic count with question count """
-    year, quarter, topic, count = int(topics[0]), int(topics[1]), topics[2], int(topics[3])
+    year, topic, count = int(topics[0]), topics[1], int(topics[2])
     for i in question_count:
         # print(i[:-1])
         # print([year, quarter])
-        if i[:-1] == [year, quarter]:
-            percentage = (count/i[2])*100
-            write_file(year, quarter, topic, count, percentage)
-            print(i[2])
+        if i[0] == year:
+            percentage = (count/i[1])*100
+            write_file(year, topic, count, percentage)
+            print(i[1])
             return
 
-def write_file(year, quarter, topic, count, percentage):
+def write_file(year, topic, count, percentage):
     """ Write data from csv reader """
     with open('./dataset/data/tag_counts/tags_count_topic_based_percentage_year.csv', 'a', encoding="utf8", newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([year, quarter, topic, count, percentage])
+        writer.writerow([year, topic, count, percentage])
     csvfile.close()
 readFile()
 # getCount()
