f2c7f3ad8c12886ea0e3d796442d66d267ad46b3
phwt.smwt <phwt.smwt@gmail.com>
2018-12-14 15:25:42 +0700

    Update topic_popularity_graph_batch.py

--- visualize/topic_popularity_graph_batch.py
+++ visualize/topic_popularity_graph_batch.py
@@ -1,24 +1,27 @@
 """ StackBehavior - Topic Popularity Graph Batch
 Visualize topic popularity data in batch into line graph format.
 """
-import pygal
 import csv
 import topic_popularity_graph
 import os
 
-output_folder = './visualize/output/topics'
+output_folder = './visualize/output/topic_popularity'
 
-def readBatch(source_folder='./dataset/data/tag_counts/topic_based_percentage'):
+def readBatch(file_format="SVG", source_folder='./dataset/data/tag_counts/topic_based_percentage'):
     """ Read each file(s) in the specific folder
         Arguments:
             `source_folder` - Folder containing source files
                               (string - default `./dataset/data/tag_counts/topic_based_percentage`)
+            `file_format` - Output file's format (`PNG` of `SVG`(default))
         Return: None
         Output: Batch of Converted Comma-Separated Values (.csv) files
     """
     for subdir, _, files in os.walk(source_folder):
         for file in files:
             path = os.path.join(subdir, file)
-            output_path = output_folder+"/"+file[:-4]+"_P"+".csv"
-            tags_percentage.convertSingle(path, output_path)
-readBatch()
+            topic = file.split("_")[1]
+            output_path = output_folder+"/"+topic+"_V"+(".svg" if file_format == "SVG" else ".png")
+            topic_popularity_graph.visualizeSingle(topic, path, output_path, file_format)
+            # print(topic, path, output_path)
+            # break
+readBatch("SVG")
