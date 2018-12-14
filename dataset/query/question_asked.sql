057fb8323982ee5beb6810ff913f2ef9b62403c9
phwt.smwt <phwt.smwt@gmail.com>
2018-12-14 20:57:01 +0700

    Comment's context convert/visualization

--- dataset/query/question_asked.sql
+++ dataset/query/question_asked.sql
@@ -1,14 +1,5 @@
 SELECT
-    total
-FROM(
-    SELECT
-        EXTRACT(YEAR FROM creation_date) as year,
-        EXTRACT(MONTH FROM creation_date) as month,
-        COUNT(*) AS total
-    FROM
-        `bigquery-public-data.stackoverflow.posts_questions`
-    WHERE EXTRACT(YEAR FROM creation_date) = 2017
-    GROUP BY year, month
-    ORDER BY year, month
-)
-GROUP BY total
+    EXTRACT(MONTH FROM creation_date) as month, COUNT(*) AS total
+FROM `bigquery-public-data.stackoverflow.posts_questions`
+GROUP BY month
+ORDER BY month
