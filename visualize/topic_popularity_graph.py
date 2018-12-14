387c76b958fd9ff06471f758065c84e0ee907423
phwt.smwt <phwt.smwt@gmail.com>
2018-12-14 15:25:40 +0700

    Update topic_popularity_graph.py

--- visualize/topic_popularity_graph.py
+++ visualize/topic_popularity_graph.py
@@ -16,12 +16,13 @@ def readConvert(path):
         readCSV = [[int(i[0]), float(i[1])] for i in readCSV]
         return list(zip(*readCSV))
 
-def visualize(title, data, output):
+def visualize(title, data, output, file_format):
     """ Visualize the given data into line graph and render to file
         Arguements:
             `title` - Graph's title (string)
             `data` - Graph's data (list)
             `output` - Output file's path (string)
+            `file_format` - Output file's format (`PNG` of `SVG`(default))
         Return: None
         Output: Vector Graphic (.svg) file
     """
@@ -29,20 +30,24 @@ def visualize(title, data, output):
     line_chart.title = title
     line_chart.x_labels = data[0]
     line_chart.add('', data[1])
-    line_chart.render_to_file(output)
+    if file_format == "SVG":
+        line_chart.render_to_file(output)
+    elif file_format == "PNG":
+        line_chart.render_to_png(output)
 
-def visualizeSingle(title, data, output):
+def visualizeSingle(title, data, output, file_format):
     """ Visualize data from a single file
         Arguments:
             `title` - Graph's title (string)
             `data` - Graph's data (list)
             `output` - Output file's path (string)
+            `file_format` - Output file's format (`PNG` of `SVG`(default))
         Return: None
         Output: Vector Graphic (.svg) file
     """
     data = readConvert(data)
-    visualize("Python", data, output)
+    visualize(title, data, output, file_format)
 
 # Below this line is for debugging purpose only
-visualizeSingle("Python", "./dataset/data/tag_counts/topic_based_percentage/TAGS_PYTHON_P.csv",
-"./visualize/output/topic_popularity/python.svg")
+# visualizeSingle("Python", "./dataset/data/tag_counts/topic_based_percentage/TAGS_PYTHON_P.csv",
+# "./visualize/output/topic_popularity/python.svg", "PNG")
